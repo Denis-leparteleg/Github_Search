c6f4ccf80a1a4fd9ce0d4e9b2b5ab8ba0ef9bb6c
Denis-Leparteleg <denislepartelegke@gmail.com>
2021-10-17 20:29:41 +0300

    add the HttpClient logic to the component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './github-search-form/navbar/navbar.component';
 import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
 import { GithubUsernameComponent } from './github-username/github-username.component';
 import { GithubRepositoriesComponent } from './github-repositories/github-repositories.component';
--- src/app/github-repositories/github-repositories.component.ts
+++ src/app/github-repositories/github-repositories.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-github-repositories',
   templateUrl: './github-repositories.component.html',
   styleUrls: ['./github-repositories.component.css']
 })
 export class GithubRepositoriesComponent implements OnInit {
+  username: any;
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    interface ApiResponse{
+     author:string
+    }
+
+    this.http.get<ApiResponse>(environment.BASEURL).subscribe(data=>{
+      // Successful API request
+      this.username = new this.username(data.author)
+    }, err=>{
+      this.username = new this.username("Denis-leparteleg")
+      console.log("An error occurred")
+    })
   }
 
 }
